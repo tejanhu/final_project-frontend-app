8f9f8f44b1cd0a97944404f68dd149d56b45c8ed
Hussein <tejanahmedhu@gmail.com>
2018-07-26 09:48:40 +0100

    adding homepage

--- src/navbar.js
+++ src/navbar.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Home from './feature/homepage/Home';
+
 
 class Navbar extends Component{
+
+
+    handleHome(){
+        ReactDOM.render(<Home/>, document.getElementById("body"));
+    }
+
 
     render(){
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Logo</a>
+                <a className="navbar-brand" onClick={this.handleHome}>Logo</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -14,7 +22,7 @@ class Navbar extends Component{
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" onClick={this.handleHome}>Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info</a>
