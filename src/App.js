354bea80b623f4aba7a215e732d87b55ab5a5bd2
George <ga.timbrell@googlemail.com>
2018-07-26 14:52:18 +0100

    added switch case for rendering different pages without calling reactDOM.render

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React, { Component } from 'react';
 import Navbar from "./feature/Shell/navbar.js";
 import Body from "./body.js";
 import Footer from "./feature/Shell/footer.js";
+import Home from "./feature/homepage/Home";
+import Game from "./feature/game/Game";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            pageName: "home",
+        };
+    }
 
+    getCurrentPage(){
+      switch(this.state.pageName) {
+          case "home":
+              return <Home handleClick={(newPage) => this.handleClick(newPage)}/>
+          case "game":
+              return <Game />
+          default:
+              return <Home />
+      }  
+    }
 
+    handleClick(newPage){
+        console.log("icon has been clicked");
+        console.log("pagename is " + newPage);
+        this.setState({
+          pageName: newPage
+        })
+    }
 
   render() {
+    console.log("App is rendering this function is rendering");
+    console.log("this.state.pageName is " + this.state.pageName);
     return (
         <div>
-            <Navbar/>
-
-            <Body/>
-
-            <Footer/>
+            <Navbar handleClick={(newPage) => this.handleClick(newPage)}/>
+            <div id ="body">
+              {this.getCurrentPage()}
+            </div>
+            <Footer handleClick={(newPage) => this.handleClick(newPage)}/>
         </div>
-
-
     );
   }
 
 }
-
 export default App;
--- src/feature/homepage/banner/Banner.js
+++ src/feature/homepage/banner/Banner.js
@@ -14,7 +14,7 @@ class Banner extends Component {
                 <div className="container">
                     <h1 className="display-3">Application Name</h1>
                     <p>Brief desciprtion of product.</p>
-                    <p><a className="btn btn-primary btn-lg" href="#" role="button" onClick={this.handleGame}>Play now! &raquo;</a></p>
+                    <p><a className="btn btn-primary btn-lg" href="#" role="button" onClick={() => this.props.handleClick("game")}>Play now! &raquo;</a></p>
                 </div>
             </div>
         );
