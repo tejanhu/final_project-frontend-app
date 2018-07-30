97ea541e82104d189be7d8d6a96e64f9a79a277a
Hussein <tejanahmedhu@gmail.com>
2018-07-30 16:09:58 +0100

    changed brief on game page

--- src/feature/homepage/info/Info.js
+++ src/feature/homepage/info/Info.js
@@ -8,12 +8,11 @@ class Info extends Component {
                 <div className="row">
                     <div className="col-sm-12 col-lg-12">
                         <h1 className="h1">
-                            About us </h1>
+                            How to Play </h1>
                         <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore
-                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                            nisi ut
-                            aliquip ex ea commodo consequat.</h4>
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                            nisi ut nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in
+                            culpa qui</h4>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                             labore
                             et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
