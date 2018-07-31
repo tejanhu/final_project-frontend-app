8be2a009e1021fa09315aa412d57e8d4edd60e35
Sam Kirk <samueljkirk10@gmail.com>
2018-07-31 09:40:52 +0100

    added content to lessons

--- src/feature/Lessons/java/main.js
+++ src/feature/Lessons/java/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Overview from "./overview";
-import Setup from "./setup";
+import DataTypes from "./datatypes";
 import Syntax from "./syntax";
 import ObjAndClass from "./objandclass";
 import '../lessons.css';
@@ -19,15 +19,15 @@ class MainJava extends Component {
                     <a className="link" data-toggle="collapse" data-parent="#collapse1" href="#overview">
                         <li style={this.itemStyle} className="list-group-item">Overview</li>
                     </a><Overview />
-                    <a className="link" data-toggle="collapse" data-parent="#collapse1" href="#setup">
-                        <li style={this.itemStyle} className="list-group-item">Set up</li>
-                    </a><Setup />
                     <a className="link" data-toggle="collapse" data-parent="#collapse1" href="#syntax">
                         <li style={this.itemStyle} className="list-group-item">Basic Syntax</li>
                     </a><Syntax />
                     <a className="link" data-toggle="collapse" data-parent="#collapse1" href="#objandclass">
                         <li style={this.itemStyle} className="list-group-item">Objects & Classes</li>
                     </a><ObjAndClass />
+                    <a className="link" data-toggle="collapse" data-parent="#collapse1" href="#datatypes">
+                        <li style={this.itemStyle} className="list-group-item">Data Types</li>
+                    </a><DataTypes />
                 </ul>
             </div>
         )
--- src/feature/Lessons/java/objandclass.js
+++ src/feature/Lessons/java/objandclass.js
@@ -5,7 +5,11 @@ class ObjAndClass extends Component {
         return (
             <div id="objandclass" className="panel-collapse collapse in">
                 <div className="panel-body">
-                    OBJECTS & CLASSES
+                    <p>In this chapter, we will look into the concepts - Classes and Objects.</p>
+                    <ul>
+                        <li><strong>Object</strong> − Objects have states and behaviors. Example: A dog has states - color, name, breed as well as behaviors – wagging the tail, barking, eating. An object is an instance of a class.</li>
+                        <li><strong>Class</strong> − A class can be defined as a template/blueprint that describes the behavior/state that the object of its type support.</li>
+                    </ul>
                 </div>
             </div>
         );
